f3611b1a410bf5ac1d0ad6c04db51c88f77fd392
llucassouz4 <lucassouzads@icloud.com>
2026-02-27 18:01:33 -0300

    att

---
+++ sporify/scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+})
+
+
+
+const artistsData = [
+    { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpg' },
+    { name: 'Jorge & Mateus', image: '.img/artista-jorge-mateus.jpg' },
+    { name: 'Zé Neto & Cristiano', image: '.img/artista-ze-neto.jpg' },
+    { name: 'Gusttavo Lima', image: '.img/artista-gustavo-limma.jpg' },
+    { name: 'Luan Santana', image: '.img/artista-luan-santana.jpg' },
+    { name: 'Matheus & Kauan', image: '.img/artista-mateus-kauan.jpg' }
+];
+
+const albumsData = [
+    { name:'White Noise (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: 'img/album-white-noise.jpg' },
+    { name:'O Céu Explica Tudo (Ao Vivo)', artist: 'img/album-ceu-explica.jpg' },
+    { name:'Nada como um dia após o outro', artist: 'Racionais', image: 'img/album-racionais.jpg' },
+    { name:'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: 'img/album-hit-me.jpg' },
+    { name:'CAJU', artist: 'Liniker', image: 'img/album-caju.jpg' },
+    { name:'Escândalo Íntimo', artist: 'Luísa Souza', image: 'img/album-escandalo.jpg' }
+];
